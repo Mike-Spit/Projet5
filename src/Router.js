e6add1701794c96a7903dd70fe052762601f1e39
Mike-Spit <mickael.spiteri.pro.com>
2025-02-13 17:07:09 +0100

    mise à jour route

--- src/Router.js
+++ src/Router.js
@@ -14,6 +14,7 @@ function Router() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
