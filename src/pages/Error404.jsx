747b047526f0e635200c9512550cef1e10aed995
Mike-Spit <mickael.spiteri.pro.com>
2025-03-14 18:14:21 +0100

    fix error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,15 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import styles from "../styles/Error404.scss";
+import "../styles/Error404.scss";
 
 export default function Error404() {
   const navigate = useNavigate();
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.container__title}>404</h1>
-      <h2 className={styles.container__description}>
+    <div className="container">
+      <h1 className="container__title">404</h1>
+      <h2 className="container__description">
         Oups! La page que vous demandez n'existe pas.
       </h2>
-      <p className={styles.container__return} onClick={() => navigate("/")}>
+      <p className="container__return" onClick={() => navigate("/")}>
         Retourner sur la page d'accueil
       </p>
     </div>
