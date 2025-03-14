1273c106263d6112ebef59fcf624e643714e8da9
Mike-Spit <mickael.spiteri.pro.com>
2025-03-14 17:23:55 +0100

    modif

--- src/components/common/Collapse.jsx
+++ src/components/common/Collapse.jsx
@@ -1,8 +1,8 @@
 // src/components/common/Collapse.jsx
 
 import React, { useState } from "react";
-import "../../styles/Collapse.scss";
 import ArrowIcon from "./ArrowIcon";
+import "../../styles/Collapse.scss";
 
 function Collapse({ title, children }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,11 +13,13 @@ function Collapse({ title, children }) {
     <div className={`collapse ${isOpen ? "is-open" : ""}`}>
       <div className="collapse__header" onClick={toggle}>
         <h2 className="collapse__title">{title}</h2>
-        <ArrowIcon className={isOpen ? "is-open" : ""} />
+        <ArrowIcon className={`collapse__arrow ${isOpen ? "is-open" : ""}`} />
       </div>
 
+      {/* 1er conteneur (animateur) */}
       <div className={`collapse__content ${isOpen ? "is-open" : ""}`}>
-        {children}
+        {/* 2e conteneur (inner) : padding, fond gris, etc. */}
+        <div className="collapse__content-inner">{children}</div>
       </div>
     </div>
   );
